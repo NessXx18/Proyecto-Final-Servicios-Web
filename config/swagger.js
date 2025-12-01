bfd9a7de9c5ea57cfccd59dc343f23b0e99dfb39
Saulo Romero <saulofco25@gmail.com>
2025-11-30 18:17:07 -0600

    Arreglado swagger para desplegar en Render

--- config/swagger.js
+++ config/swagger.js
@@ -1,24 +1,26 @@
+const path = require('path');
 const swaggerJsdoc = require('swagger-jsdoc');
 
 const options = {
-    definition: {
-        openapi: '3.0.0',
-        info: {
-            title: 'SmartCity Lab - Plataforma IoT API',
-            version: '1.0.0',
-            description: 'API Backend para la Plataforma IoT SmartCity Lab',
-            contact: {
-                name: 'Soporte SmartCity Lab',
-            },
-        },
-        servers: [
-            {
-                url: 'http://localhost:3000',
-                description: 'Servidor de desarrollo',
-            },
-        ],
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'SmartCity Lab - Plataforma IoT API',
+      version: '1.0.0',
+      description: 'API Backend para la Plataforma IoT SmartCity Lab',
+      contact: { name: 'Soporte SmartCity Lab' },
     },
-    apis: ['./routes/*.js', './models/*.js'], // Files containing annotations
+    servers: [
+      {
+        url: 'https://proyecto-final-servicios-web.onrender.com',
+        description: 'Servidor producción',
+      },
+    ],
+  },
+  apis: [
+    path.join(__dirname, '../routes/*.js'),   // Ajusta según tu estructura
+    path.join(__dirname, '../models/*.js'),
+  ],
 };
 
 const specs = swaggerJsdoc(options);
